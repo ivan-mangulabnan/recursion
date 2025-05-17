ff3ed7504292ba5152c9408467b487ca6b28acd1
ivan <ivanmangulabnan19@gmail.com>
2025-05-17 11:33:39 +0800

    Add the interation version of fibonacci

---
+++ fibonacci.js
@@ -0,0 +1,14 @@
+// Using iteration.
+function fibs (n) {
+  if (n === 0) return [];
+  if (n === 1) return [0];
+  if (n === 2) return [0, 1];
+
+  let arr = [0, 1];
+
+  for (let i = 2; i < n; i++) {
+    arr.push(arr[i - 1] + arr[i - 2]);
+  }
+
+  return arr;
+}
