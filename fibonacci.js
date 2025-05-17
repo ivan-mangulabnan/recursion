45d3bd277517634ed582f517ee99f337abcd486b
ivan <ivanmangulabnan19@gmail.com>
2025-05-17 11:37:09 +0800

    Add the recursive ver of fibonacci

--- fibonacci.js
+++ fibonacci.js
@@ -12,3 +12,17 @@ function fibs (n) {
 
   return arr;
 }
+
+// Using recursive
+function fibnRec (n) {
+  if (n === 0) return [];
+  if (n === 1) return [0];
+  if (n === 2) return [0, 1];
+
+  let prev = fibnRec(n - 1);
+  let current = prev[prev.length - 1] + prev[prev.length - 2];
+  prev.push(current);
+  return prev;
+}
+
+console.log(fibnRec(8));
